a2459f9288ba97964181c3646058742e840e9836
MARIA GONZALEZ <maria.gonzalez@civica-soft.com>
2024-06-19 17:19:46 +0000

    fact_budget completed

--- models/marts/core/ventas/fact_budget.sql
+++ models/marts/core/ventas/fact_budget.sql
@@ -1,27 +1,26 @@
-with stg_budget as(
-
+with stg_budget as (
     select * from {{ ref('stg_google_sheets__budget') }}
 ),
 
 dim_products as (
-    from {{ ref('dim_products') }}
+    select * from {{ ref('dim_products') }}
 ),
 
 dim_date as (
-    from {{ ref('dim_date') }}
+    select * from {{ ref('dim_date') }}
 ),
 
-fact_budget as(
-    
+fact_budget as (
     select
         a.budget_id,
         b.product_id,
         a.quantity,
         c.date_key
-
     from stg_budget a
     left join dim_products b on b.product_id = a.product_id
-    left join dim_date c on c.month_of_year = a.date
+    left join dim_date c on c.month_of_year = EXTRACT(month FROM a.date)
 )
 
-SELECT * FROM fact_budget ORDER BY 4 -- ordena los resultados por date_key
+select * 
+from fact_budget 
+order by 4  -- ordena los resultados por date_key
